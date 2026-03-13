a165f7c2b4c14d39d5c7c16fd98171185316622c
jorhdan <dil@DiLs-MacBook-Air.local>
2026-03-13 09:55:24 +0300

    fix error

--- config/settings.py
+++ config/settings.py
@@ -122,8 +122,8 @@ DATABASES = {
         'NAME': config('DB_NAME'),
         'USER': config('DB_USER'),
         'PASSWORD': config('DB_PASSWORD'),
-        'HOST': config('DB_HOST', default='127.0.0.1'),
-        'PORT': config('DB_PORT', default='5432'),
+        'HOST': config('DB_HOST'),
+        'PORT': config('DB_PORT'),
     }
 }
 
