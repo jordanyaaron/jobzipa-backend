a49180b0daaab2b4642615951c7b603ddd52cd7a
jorhdan <dil@192.168.1.7>
2026-04-13 07:10:26 +0300

    import datetime and time

--- api/v1/jobs_api/views.py
+++ api/v1/jobs_api/views.py
@@ -4,6 +4,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated , AllowAny
+from datetime import datetime, time
 
 from .utils.storage import upload_logo_to_bucket
 
