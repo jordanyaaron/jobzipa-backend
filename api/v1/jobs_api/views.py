bb9ed248b5b1ea1da52a56cdd53532a9acbaf3ad
jorhdan <dil@DiLs-MacBook-Air.local>
2026-04-15 14:59:22 +0300

    fix NameError: name 'RetrieveAPIView' is not defined

--- api/v1/jobs_api/views.py
+++ api/v1/jobs_api/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth import get_user_model
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.generics import RetrieveAPIView
 from rest_framework.permissions import IsAuthenticated , AllowAny
 from datetime import datetime, time
 from django.utils import timezone
