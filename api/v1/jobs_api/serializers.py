619dd95c7a6f35b063d2743735f5c5847c339a71
jorhdan <dil@192.168.1.7>
2026-04-19 15:53:04 +0300

    modify serializer

--- api/v1/jobs_api/serializers.py
+++ api/v1/jobs_api/serializers.py
@@ -2,7 +2,10 @@ from rest_framework import serializers
 from django.contrib.auth import authenticate
 from apps.jobs.models import Job
 
+
 class JobSerializer(serializers.ModelSerializer):
+    company_logo = serializers.CharField(required=False, allow_null=True, write_only=True)
+
     class Meta:
         model = Job
         fields = "__all__"
