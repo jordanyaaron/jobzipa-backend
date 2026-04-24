120a030794a7d9be26e71f3be0954327106cbf82
jorhdan <dil@192.168.1.7>
2026-04-24 12:32:33 +0300

    add configured enviroment variable

--- config/settings.py
+++ config/settings.py
@@ -19,6 +19,58 @@ ALLOWED_HOSTS = config("ALLOWED_HOSTS").split(",")
 
 AUTH_USER_MODEL = 'users.User'
 
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
+DEBUG = config("DEBUG", default=False, cast=bool)
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+FRONTEND_WEB_APP_NAME="Jobzipa"
+
+# superuser info
+DJANGO_SUPERUSER_USERNAME=config('DJANGO_SUPERUSER_USERNAME')
+DJANGO_SUPERUSER_EMAIL=config('DJANGO_SUPERUSER_EMAIL')
+DJANGO_SUPERUSER_PASSWORD=config('DJANGO_SUPERUSER_PASSWORD')
+
+# amazon
+AWS_ACCESS_KEY_ID = config("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = config("AWS_SECRET_ACCESS_KEY")
+AWS_REGION = config("AWS_REGION")
+AWS_S3_BUCKET_NAME = config("AWS_S3_BUCKET_NAME")
+AWS_S3_BASE_URL = config("AWS_S3_BASE_URL")
+
+# email
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_HOST = config("EMAIL_HOST")
+EMAIL_PORT = config("EMAIL_PORT", cast=int)
+EMAIL_USE_TLS = config("EMAIL_USE_TLS", cast=bool)
+EMAIL_HOST_USER = config("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")
+DEFAULT_FROM_EMAIL = config("DEFAULT_FROM_EMAIL")
+
+
+
+FRONTEND_URL=config("FRONTEND_URL")
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = config("SECRET_KEY")
+
+AUTH_USER_MODEL = 'users.User'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = config('DEBUG', default=False, cast=bool)
+
+AWS_ACCESS_KEY_ID = config("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = config("AWS_SECRET_ACCESS_KEY")
+AWS_REGION = config("AWS_REGION")
+AWS_S3_BUCKET_NAME = config("AWS_S3_BUCKET_NAME")
+# Application definition
+
 # =========================
 # 🔥 APPS
 # =========================
