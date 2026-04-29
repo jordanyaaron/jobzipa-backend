5f7263e9076cc6b703436f4b3243bc0c9c9b6352
jorhdan <dil@192.168.1.9>
2026-04-29 22:46:03 +0300

    update the url

--- api/v1/jobs_api/urls.py
+++ api/v1/jobs_api/urls.py
@@ -10,7 +10,7 @@ from .views import(
 urlpatterns = [
     path("create/", JobCreateView.as_view(), name="create-job"),
     path("create/more/", JobCreateMoreView.as_view(), name="create-more"),
-    path("update/", JobFullUpdateView.as_view(), name="update-job"),
+    path("update/<uuid:public_id>/", JobFullUpdateView.as_view(), name="update-job"),
     path("get/", JobListView.as_view(), name="get-jobs"),
     path("<uuid:public_id>/", JobDetailView.as_view(), name="job-detail"),
 ]
