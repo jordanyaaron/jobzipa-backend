c5bbb6d87b40a7421ef47b34c61750995df384b2
jorhdan <dil@192.168.1.9>
2026-04-29 23:27:58 +0300

    try to fix jobs.vacancie in exist

--- api/v1/jobs_api/views.py
+++ api/v1/jobs_api/views.py
@@ -315,6 +315,8 @@ class JobListView(APIView):
 
         serializer = JobSerializer(jobs, many=True)
         return Response(serializer.data)
+
+        
 class JobDetailView(RetrieveAPIView):
     queryset = Job.objects.filter(is_active=True)
     serializer_class = JobSerializer
